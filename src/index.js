91b5131ecd789db50d3bcbb207590933f7f9ba99
Govinda Sarkhel <gsarkhel@mitrmedia.com>
2020-09-29 11:28:31 +0530

    Clean

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Controller from './main/controller';
-ReactDOM.render(
-    <Controller />,
-    document.getElementById('root')
-  );
