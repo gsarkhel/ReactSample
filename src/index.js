4f1d7113c17fc9b272941228a652bd2822c02a91
Govinda Sarkhel <gsarkhel@mitrmedia.com>
2020-09-29 11:31:09 +0530

    Re-uploaded the files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Controller from './main/controller';
+ReactDOM.render(
+  <Controller />,
+  document.getElementById('root')
+);
---
+++ src/main/controller.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../css/index.css';
+
+export default class Controller extends React.Component {
+    render() {
+        return (
+            <div>Hello</div>
+        );
+    }
+}
